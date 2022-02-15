a51890f4548dcbff48dce44fdbc8e8279c6f6720
Denys_Moiseienko <denys.moiseienko1@gmail.com>
2022-02-15 10:50:04 +0100

    update unit tests for ProdutServiceImp

--- src/test/java/academy/productstore/service/ProductServiceImpUnitTest.java
+++ src/test/java/academy/productstore/service/ProductServiceImpUnitTest.java
@@ -54,11 +54,13 @@ class ProductServiceImpUnitTest {
     }
 
     @Test
-    void getProductsByCategory() {
+    void getProductsByCategory_shouldReturnsProductsList() {
         // given
+        var category = createTestCategory(1L, "Beverages");
         List<Product> list = List.of(
-                createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5)),
-                createTestProduct(3, "Pepsi", "", BigDecimal.valueOf(0.45)));
+                createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), category),
+                createTestProduct(3, "Pepsi", "", BigDecimal.valueOf(0.45), category)
+        );
         Pageable pageable = PageRequest.of(0,20);
         Page<Product> products = new PageImpl<>(list, pageable, 1);
         given(mockRepository.findProductsByCategoryId(1, pageable)).willReturn(products);
@@ -68,14 +70,16 @@ class ProductServiceImpUnitTest {
 
         // then
         assertEquals(2, actual.getTotalElements());
+        assertEquals(1, actual.getContent().get(0).getCategory().getId());
         verify(mockRepository, times(1)).findProductsByCategoryId(1, pageable);
     }
 
     @Test
-    void search() {
+    void search_shouldReturnsProductsList() {
         // given
+        var category = createTestCategory(1L, "Beverages");
         List<Product> list = List.of(
-                createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5)));
+                createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), category));
         Pageable pageable = PageRequest.of(0,20);
         Page<Product> products = new PageImpl<>(list, pageable, 1);
         given(mockRepository.search("Co", pageable)).willReturn(products);
@@ -85,6 +89,7 @@ class ProductServiceImpUnitTest {
 
         //then
         assertEquals(1, actual.getTotalElements());
+        assertEquals(1, actual.getContent().get(0).getCategory().getId());
         verify(mockRepository, times(1)).search("Co", pageable);
     }
 
@@ -104,7 +109,8 @@ class ProductServiceImpUnitTest {
     @Test()
     void getProductById_shouldReturnsProduct() {
         // given
-        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5));
+        var category = createTestCategory(1L, "Beverages");
+        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), category);
         given(mockRepository.findProductById(product.getId())).willReturn(product);
 
         // when
@@ -120,7 +126,7 @@ class ProductServiceImpUnitTest {
     @Test
     void addProduct_shouldReturnsProduct() {
         // given
-        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5));
+        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), null);
         given(mockRepository.save(any(Product.class))).willReturn(product);
 
         // when
@@ -130,36 +136,31 @@ class ProductServiceImpUnitTest {
         assertEquals(2, actual.getId());
         assertEquals("Coca-Cola", actual.getName());
         assertEquals(BigDecimal.valueOf(0.5), actual.getPrice());
-        assertEquals(1, actual.getCategory().getId());
-        verify(mockCategoryService, times(1)).getCategoryById(product.getCategory().getId());
         verify(mockRepository, times(1)).save(any(Product.class));
     }
 
     @Test
     void updateProduct_shouldReturnsProduct() {
         // given
-        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5));
+        var product = createTestProduct(1, "Pepsi", "Diet", BigDecimal.valueOf(0.5), null);
         given(mockRepository.findProductById(product.getId())).willReturn(product);
-        given(mockCategoryService.getCategoryById(product.getCategory().getId())).willReturn(product.getCategory());
         given(mockRepository.save(any(Product.class))).willReturn(product);
 
         // when
         Product actual = service.updateProduct(product, product.getId());
 
         // then
-        assertEquals(2, actual.getId());
-        assertEquals("Coca-Cola", actual.getName());
+        assertEquals(1, actual.getId());
+        assertEquals("Pepsi", actual.getName());
         assertEquals(BigDecimal.valueOf(0.5), actual.getPrice());
-        assertEquals(1, actual.getCategory().getId());
         verify(mockRepository, times(1)).findProductById(product.getId());
-        verify(mockCategoryService, times(1)).getCategoryById(product.getCategory().getId());
         verify(mockRepository, times(1)).save(any(Product.class));
     }
 
     @Test
     void updateProduct_shouldThrowsEntityNotFoundException() {
         // given
-        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5));
+        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), null);
 
         // when
         EntityNotFoundException exception =
@@ -168,14 +169,13 @@ class ProductServiceImpUnitTest {
         // then
         assertEquals("Product not found", exception.getMessage());
         verify(mockRepository, times(1)).findProductById(1);
-        verify(mockCategoryService, times(0)).getCategoryById(product.getCategory().getId());
         verify(mockRepository, times(0)).save(any(Product.class));
     }
 
     @Test
     void deleteProduct() {
         // given
-        var product = createTestProduct(2, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5));
+        var product = createTestProduct(2, "Coca-Cola", "", BigDecimal.valueOf(0.5), null);
         given(mockRepository.findProductById(product.getId())).willReturn(product);
 
         // when
@@ -200,18 +200,132 @@ class ProductServiceImpUnitTest {
         verify(mockRepository, times(0)).delete(any(Product.class));
     }
 
-    private Product createTestProduct(long id, String name, String desc, BigDecimal price) {
-        var category = new Category();
-        category.setId(1L);
-        category.setName("Beverages");
+    @Test
+    void addProductToCategory_WhenProductIsNotFound_shouldThrowsEntityNotFoundException() {
+        // given
 
+        // when
+        EntityNotFoundException exception =
+                assertThrows(EntityNotFoundException.class, () -> service.addProductToCategory(1, 2));
+
+        // then
+        assertEquals("Product not found", exception.getMessage());
+        verify(mockRepository, times(1)).findProductById(1);
+        verify(mockCategoryService, times(0)).getCategoryById(1);
+        verify(mockRepository, times(0)).save(any(Product.class));
+    }
+
+    @Test
+    void addProductToCategory_WhenCategoryIsNotFound_shouldThrowsEntityNotFoundException() {
+        // given
+        var product = createTestProduct(1, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), null);
+        given(mockRepository.findProductById(product.getId())).willReturn(product);
+        given(mockCategoryService.getCategoryById(2)).willThrow(new EntityNotFoundException("Category not found"));
+
+        // when
+        EntityNotFoundException exception =
+                assertThrows(EntityNotFoundException.class, () -> service.addProductToCategory(product.getId(), 2));
+
+        // then
+        assertEquals("Category not found", exception.getMessage());
+        verify(mockRepository, times(1)).findProductById(1);
+        verify(mockCategoryService, times(1)).getCategoryById(2);
+        verify(mockRepository, times(0)).save(any(Product.class));
+    }
+
+    @Test
+    void addProductToCategory_shouldReturnsProductWithUpdatedCategory() {
+        // given
+        var category = createTestCategory(2L, "Beverages");
+        var product = createTestProduct(1, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), null);
+        var expected = createTestProduct(1, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), category);
+        given(mockRepository.findProductById(product.getId())).willReturn(product);
+        given(mockCategoryService.getCategoryById(category.getId())).willReturn(category);
+        given(mockRepository.save(any(Product.class))).willReturn(expected);
+
+        // when
+        var actual = service.addProductToCategory(product.getId(), category.getId());
+
+        // then
+        assertEquals(1, actual.getId());
+        assertEquals("Coca-Cola", actual.getName());
+        assertEquals(BigDecimal.valueOf(0.5), actual.getPrice());
+        assertEquals(2, actual.getCategory().getId());
+        assertEquals("Beverages", actual.getCategory().getName());
+        verify(mockRepository, times(1)).findProductById(1);
+        verify(mockCategoryService, times(1)).getCategoryById(2);
+        verify(mockRepository, times(1)).save(any(Product.class));
+    }
+
+    @Test
+    void addProductToCategory_shouldReturnsProductWithNotUpdatedCategory() {
+        // given
+        var category = createTestCategory(2L, "Dairy products");
+        var product = createTestProduct(1, "Milk", "1L, 3.2%", BigDecimal.valueOf(0.5), category);
+        given(mockRepository.findProductById(product.getId())).willReturn(product);
+        given(mockCategoryService.getCategoryById(category.getId())).willReturn(category);
+
+        // when
+        var actual = service.addProductToCategory(product.getId(), category.getId());
+
+        // then
+        assertEquals(1, actual.getId());
+        assertEquals("Milk", actual.getName());
+        assertEquals(BigDecimal.valueOf(0.5), actual.getPrice());
+        assertEquals(2, actual.getCategory().getId());
+        assertEquals("Dairy products", actual.getCategory().getName());
+        verify(mockRepository, times(1)).findProductById(1);
+        verify(mockCategoryService, times(1)).getCategoryById(2);
+        verify(mockRepository, times(0)).save(any(Product.class));
+    }
+
+    @Test
+    void removeProductFromCategory_WhenProductDoesNotExist_shouldThrowsEntityNotFoundException() {
+        // given
+
+        // when
+        EntityNotFoundException exception =
+                assertThrows(EntityNotFoundException.class, () -> service.removeProductFromCategory(1, 2));
+
+        // then
+        assertEquals("Product not found", exception.getMessage());
+        verify(mockRepository, times(1)).findProductById(1);
+        verify(mockCategoryService, times(0)).getCategoryById(1);
+        verify(mockRepository, times(0)).save(any(Product.class));
+    }
+
+    @Test
+    void removeProductFromCategory_WhenCategoryDoesNotExist_shouldThrowsEntityNotFoundException() {
+        // given
+        var product = createTestProduct(1, "Coca-Cola", "Diet", BigDecimal.valueOf(0.5), null);
+        given(mockRepository.findProductById(product.getId())).willReturn(product);
+        given(mockCategoryService.getCategoryById(2)).willThrow(new EntityNotFoundException("Category not found"));
+
+        // when
+        EntityNotFoundException exception =
+                assertThrows(EntityNotFoundException.class, () -> service.removeProductFromCategory(product.getId(), 2));
+
+        // then
+        assertEquals("Category not found", exception.getMessage());
+        verify(mockRepository, times(1)).findProductById(1);
+        verify(mockCategoryService, times(1)).getCategoryById(2);
+        verify(mockRepository, times(0)).save(any(Product.class));
+    }
+
+    private Product createTestProduct(long id, String name, String desc, BigDecimal price, Category category) {
         var product = new Product();
         product.setId(id);
         product.setName(name);
         product.setDescription(desc);
         product.setPrice(price);
         product.setCategory(category);
-
         return product;
+    }
+
+    private Category createTestCategory(long id, String name) {
+        var category = new Category();
+        category.setId(id);
+        category.setName(name);
+        return category;
     }
 }
